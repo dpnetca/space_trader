7aa639ef59e95a2429dea629c648b0c164f5afc0
dpnetca <git@dpnet.ca>
2023-08-12 10:05:52 -0600

    renamed methods

--- space_traders/api/agent_api.py
+++ space_traders/api/agent_api.py
@@ -11,26 +11,26 @@ class AgentApi:
         self.client = client
         self.base_endpoint = "/agents"
 
-    def get_my_agent(self) -> Agent | ApiError:
+    def get_me(self) -> Agent | ApiError:
         r = self.client.send("get", "/my/agent")
         if "error" in r.keys():
             return ApiError(**r)
         return Agent(**r)
 
-    def get_agent(self, symbol: str) -> Agent | ApiError:
+    def get(self, symbol: str) -> Agent | ApiError:
         endpoint = self.base_endpoint + f"/{symbol}"
         r = self.client.send("get", endpoint)
         if "error" in r.keys():
             return ApiError(**r)
         return Agent(**r)
 
-    def list_agents(self, limit=20, page=1) -> List[Agent] | ApiError:
+    def list(self, limit=20, page=1) -> List[Agent] | ApiError:
         params = {"limit": limit, "page": page}
         r = self.client.send("get", self.base_endpoint, params=params)
         if "error" in r.keys():
             return ApiError(**r)
         return [Agent(**a) for a in r["data"]]
 
-    def list_all_agents(self) -> List[Agent] | ApiError:
+    def list_all(self) -> List[Agent] | ApiError:
         agents = paginator(self.client, "get", self.base_endpoint)
         return agents
