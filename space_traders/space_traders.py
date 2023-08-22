102f796154d30c21b29f4739271b307fcb795e25
dpnetca <git@dpnet.ca>
2023-08-22 03:35:24 +0000

    remove commented code no longer required

--- space_traders/space_traders.py
+++ space_traders/space_traders.py
@@ -18,17 +18,6 @@ class SpaceTrader:
         self.ship = ShipApi(self.client)
         self.system = SystemApi(self.client)
 
-    # def agent_api(self):
-    #     return AgentApi(self.client)
-
-    # def contract_api(self):
-    #     return ContractApi(self.client)
-
-    # def ship_api(self):
-    #     return ShipApi(self.client)
-
-    # def system_api(self):
-    #     return SystemApi(self.client)
     async def __aenter__(self):
         return self
 
