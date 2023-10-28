19b4c32831c74341d11ad35390ecb2f80a90397a
dpnetca <git@dpnet.ca>
2023-10-28 23:10:50 +0000

    added testing with auth and without auth token

--- tests/test_client.py
+++ tests/test_client.py
@@ -20,6 +20,25 @@ class TestClient:
         response = await st.client.send("get", "", auth=False)
         assert isinstance(response, dict)
 
+    @pytest.mark.asyncio
+    async def test_authorized_send_get(self, st: SpaceTrader):
+        st.client.token = "valid"
+        response = await st.client.send("get", "/my/agent")
+        assert isinstance(response, dict)
+
+    @pytest.mark.asyncio
+    async def test_unauthorized_get(self, st: SpaceTrader):
+        response = await st.client._get(st.client.base_url + "/my/agent")
+        assert response.status_code == 401
+
+    @pytest.mark.asyncio
+    async def test_authorized_get(self, st: SpaceTrader):
+        headers = {"Authorization": f"Bearer xxx"}
+        response = await st.client._get(
+            st.client.base_url + "/my/agent", headers
+        )
+        assert response.status_code == 200
+
     # mock server does not respond the same as actual server...not really valid
     # @pytest.mark.asyncio
     # async def test_invalid_send_get(self, st: SpaceTrader):
@@ -43,3 +62,10 @@ class TestClient:
     #         },
     #     }
     #     assert response == expected_response
+
+    # mock server differs...how to test this???
+    # @pytest.mark.asyncio
+    # async def test_unauthorized_send_get(self, st: SpaceTrader):
+    #     st.client.token = None
+    #     response = await st.client.send("get", "/my/agent", auth=False)
+    # ...
