9d5162eb5e56a07eabf21baa33b6e38d8a5c8ec9
dpnetca <git@dpnet.ca>
2023-10-28 21:25:37 +0000

    updated agents api models, added comments and removed  method which was an older vsersion of

--- space_traders/api/agent_api.py
+++ space_traders/api/agent_api.py
@@ -11,32 +11,37 @@ class AgentApi:
         self.base_endpoint = "/agents"
 
     async def get_my_agent(self) -> Agent | ApiError:
+        """Get agent assigned to API Token
+
+        Returns:
+            Agent | ApiError: Agent details
+        """
         response = await self.client.send("get", "/my/agent")
         if "error" in response.keys():
             return ApiError(**response)
         return Agent(**response["data"])
 
     async def get_agent(self, symbol: str) -> Agent | ApiError:
+        """Get public agent details for passed in agent symbol
+
+        Args:
+            symbol (str): agent symbol
+
+        Returns:
+            Agent | ApiError: Public agent details
+        """
         endpoint = self.base_endpoint + f"/{symbol}"
         response = await self.client.send("get", endpoint)
         if "error" in response.keys():
             return ApiError(**response)
         return Agent(**response["data"])
 
-    async def list_agents(
-        self, limit: int = 20, page: int = 1
-    ) -> ListAgents | ApiError:
-        params = {"limit": limit, "page": page}
-        response = await self.client.send(
-            "get", self.base_endpoint, params=params
-        )
-        if "error" in response.keys():
-            return ApiError(**response)
-        agents = [Agent(**a) for a in response["data"]]
-        meta = Meta(**response["meta"])
-        return ListAgents(data=agents, meta=meta)
-
     async def list_all_agents(self) -> List[Agent] | ApiError:
+        """get a list of all agents public agent details
+
+        Returns:
+            List[Agent] | ApiError: list of public agent details
+        """
         response = await paginator(self.client, "get", self.base_endpoint)
         if isinstance(response, ApiError):
             return response
