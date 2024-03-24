5ff26b351aec00d48ed6686e543a359bc0a2bff1
dpnetca <git@dpnet.ca>
2024-03-24 18:01:37 +0000

    ignore extra fields in pydantic models and increment version

--- space_traders/models/base_model.py
+++ space_traders/models/base_model.py
@@ -2,4 +2,5 @@ from pydantic import BaseModel, ConfigDict
 
 
 class Base(BaseModel):
-    model_config = ConfigDict(extra="forbid")
+    # model_config = ConfigDict(extra="forbid")
+    model_config = ConfigDict(extra="ignore")
